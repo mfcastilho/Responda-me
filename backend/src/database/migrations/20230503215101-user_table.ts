90fb139da65ab0e967863888122d99ed944ff652
mfcastilho <fredericocastilho@hotmail.com>
2023-05-03 19:19:16 -0300

    feat: creating the database with sequelize

---
+++ backend/src/database/migrations/20230503215101-user_table.ts
@@ -0,0 +1,39 @@
+import { QueryInterface, DataTypes } from 'sequelize';
+
+module.exports = {
+  up: async (queryInterface: QueryInterface) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface: QueryInterface) => {
+    await queryInterface.dropTable('users');
+  },
+};
