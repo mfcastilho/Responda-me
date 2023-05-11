e2e06224f76712c3ae052c5fe7fb1e04b2d4c57b
mfcastilho <fredericocastilho@hotmail.com>
2023-05-10 21:03:46 -0300

    feat: implementing getUserInfos method

--- backend/src/controllers/UserControllers.ts
+++ backend/src/controllers/UserControllers.ts
@@ -15,6 +15,30 @@ class UserController {
    
      public async getUserInfos(req: Request, res: Response){
 
+          try {
+               
+               const { id } = req.params;
+               const user = await UserModel.findByPk(id);
+
+               if(!user){
+                    return res.status(400).json({message:"Usuário não pode ser encontrado."});
+               }
+
+               return res.status(200).json({data:user});
+
+          } catch (error) {
+                
+               if(error instanceof ConnectionRefusedError){
+                    return res.status(500).json({error: true, message: "Sistema indisponível, tente novamente mais tarde!"})
+               }
+               if(error instanceof UniqueConstraintError){
+                    return res.status(400).json(error.parent.message);
+               }
+               if(error instanceof ValidationError){
+                    return res.status(400).json({error: true, message: `${error.errors[0].type} at ${error.errors[0].path}`})
+               }
+               
+          }
      }
 
      public async getAllUsers(req: Request, res: Response){
